a57565d9ce4052434d6b1721486869549753b111
pelillian <plillian@usc.edu>
2019-02-11 23:01:49 -0800

    Typo in encode_gifs

--- utils/images.py
+++ utils/images.py
@@ -3,7 +3,7 @@ def encode_gif(images, fps):
     if len(images[0].shape) == 3:
         h, w, c = images[0].shape
     else:
-        h, w = image[0].shape
+        h, w = images[0].shape
         c = 1
     cmd = ['ffmpeg', '-y',
            '-f', 'rawvideo',
