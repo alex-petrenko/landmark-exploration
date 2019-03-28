6ee11bec28229e41257e30579d63a2c12b0d0f1e
Gautam Salhotra <gautams3@users.noreply.github.com>
2019-03-27 18:56:17 -0700

    Fixing unit test failures

--- algorithms/topological_maps/tests/test_topological.py
+++ algorithms/topological_maps/tests/test_topological.py
@@ -180,17 +180,11 @@ class TestGraph(TestCase):
         for i in range(1, 4):
             for j in range(1, 4):
                 m.add_landmark(np.array(0), {'pos': {'agent_x': 300 + 50*i, 'agent_y': 400 + 100*j, 'agent_a': 10}})
-        m.add_landmark(np.array(0), {'pos': {'agent_x': 0, 'agent_y': 0, 'agent_a': 10}})
-        m.add_landmark(np.array(0), {'pos': {'agent_x': 928, 'agent_y': 928, 'agent_a': 10}})
-        m.add_landmark(np.array(0), {'pos': {'agent_x': 1856, 'agent_y': 1856, 'agent_a': 10}})
 
-        import cv2
-        img = cv2.imread("/home/gautam/curiosity/landmark-exploration/playground/doom_textured_easy.png")
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         graph = m.labeled_graph
-        figure = plot_graph(graph, map_img=img, layout='pos')
+        figure = plot_graph(graph, layout='pos')
 
-        show = True
+        show = False
         if show:
             from matplotlib import pyplot as plt
             plt.show()
