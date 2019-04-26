f1c3513032852f263c903ea0ab3e728b2e93e71c
Aleksei Petrenko <petrenko@usc.edu>
2019-04-25 22:04:09 -0700

    Add experimental environments

--- utils/envs/doom/doom_utils.py
+++ utils/envs/doom/doom_utils.py
@@ -48,7 +48,12 @@ DOOM_ENVS = [
     DoomCfg('doom_textured_very_sparse', 'VizdoomTexturedMazeVerySparse-v0', 1.0, 2100),
     DoomCfg('doom_textured', 'VizdoomTexturedMaze-v0', 1.0, 2100),
     DoomCfg('doom_textured_multi_goal', 'VizdoomTexturedMazeMultiGoal-v0', 1.0, 2100),
+
     DoomCfg('doom_textured_super_sparse', 'VizdoomTexturedMazeSuperSparse-v0', 1.0, 2100),
+    DoomCfg('doom_textured_super_sparse_4200', 'VizdoomTexturedMazeSuperSparse-v0', 1.0, 4200),
+    DoomCfg('doom_textured_super_sparse_8400', 'VizdoomTexturedMazeSuperSparse-v0', 1.0, 8400),
+    DoomCfg('doom_textured_super_sparse_16800', 'VizdoomTexturedMazeSuperSparse-v0', 1.0, 16800),
+
     DoomCfg('doom_textured_large_no_goal', 'VizdoomTexturedMazeLargeNoGoal-v0', 1.0, 2100),
 ]
 
