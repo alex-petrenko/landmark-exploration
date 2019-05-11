56f3fb1c7d62b32a80303920bdba87849769dfc2
Aleksei Petrenko <petrenko@usc.edu>
2019-05-10 20:27:05 -0700

    New textured multi goal env

--- utils/envs/doom/doom_utils.py
+++ utils/envs/doom/doom_utils.py
@@ -47,7 +47,6 @@ DOOM_ENVS = [
     DoomCfg('doom_textured_easy', 'VizdoomTexturedMazeEasy-v0', 1.0, 2100),
     DoomCfg('doom_textured_very_sparse', 'VizdoomTexturedMazeVerySparse-v0', 1.0, 2100),
     DoomCfg('doom_textured', 'VizdoomTexturedMaze-v0', 1.0, 2100),
-    DoomCfg('doom_textured_multi_goal', 'VizdoomTexturedMazeMultiGoal-v0', 1.0, 2100),
 
     DoomCfg('doom_textured_super_sparse', 'VizdoomTexturedMazeSuperSparse-v0', 1.0, 20000),
     DoomCfg('doom_textured_super_sparse_4200', 'VizdoomTexturedMazeSuperSparse-v0', 1.0, 4200),
@@ -55,6 +54,7 @@ DOOM_ENVS = [
     DoomCfg('doom_textured_super_sparse_16800', 'VizdoomTexturedMazeSuperSparse-v0', 1.0, 16800),
 
     DoomCfg('doom_textured_super_sparse_v2', 'VizdoomTexturedMazeSuperSparse-v2', 1.0, 20000),
+    DoomCfg('doom_textured_multi_goal', 'VizdoomTexturedMazeMultiGoal-v0', 1.0, 20000),
 
     DoomCfg('doom_textured_large_no_goal', 'VizdoomTexturedMazeLargeNoGoal-v0', 1.0, 2100),
     DoomCfg('doom_textured_no_goal_random', 'VizdoomTexturedMazeNoGoalRandom-v0', 1.0, 20000),
--- utils/envs/doom/play_doom.py
+++ utils/envs/doom/play_doom.py
@@ -4,7 +4,7 @@ from utils.envs.envs import create_env
 
 
 def main():
-    env = create_env('doom_textured_no_goal_random', mode='test', show_automap=True)
+    env = create_env('doom_textured_multi_goal', mode='test', show_automap=True)
     return env.unwrapped.play_human_mode()
 
 
